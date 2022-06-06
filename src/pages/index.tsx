de8cf477cbacbb7d48c74dccda1664eb1767b548
Rimarok <romaric.ruga@gmail.com>
2022-06-06 11:26:41 +0200

    index page: integrate Layout component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,15 @@
 import * as React from "react";
+import Layout from "../components/Layout";
 import "../styles/global.scss";
 
 const Accueil = () => {
   return (
-    <div>
-      <h1>Moins égal plus</h1>
-      <p>RIMAROK, ingénierie web et eco-conception</p>
-    </div>
+    <Layout>
+      <>
+        <h1>Moins égal plus</h1>
+        <p>RIMAROK, ingénierie web et eco-conception</p>
+      </>
+    </Layout>
   );
 };
 
