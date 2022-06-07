4a4bed11e54b5ba3ee2bb6d0b11f35c7089d88bd
Rimarok <romaric.ruga@gmail.com>
2022-06-07 07:43:24 +0200

    Layout.tsx: add head prop + Helmet integration

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,36 @@
 import React, { ReactElement } from "react";
+import { Helmet } from "react-helmet";
+import WEBSITE_DATA from "../data/website";
 
 type LayoutProps = {
+  head: {
+    /**
+     * Only the page title;
+     *  site title will be added for a final shape of
+     *  `{pageTitle} - {WEBSITE_DATA.TITLE}`.
+     */
+    pageTitle: string;
+    /**
+     * If set, includes
+     *  `<meta name="robots" content="noindex" />`.
+     */
+    noIndex?: true;
+  };
   children: ReactElement;
 };
 
-function Layout({ children }: LayoutProps) {
-  return <div className="container-fluid">{children}</div>;
+function Layout({ head: { pageTitle, noIndex }, children }: LayoutProps) {
+  return (
+    <>
+      <Helmet>
+        <title>
+          {pageTitle} - {WEBSITE_DATA.TITLE}
+        </title>
+        {noIndex && <meta name="robots" content="noindex" />}
+      </Helmet>
+      <div className="container-fluid">{children}</div>
+    </>
+  );
 }
 
 export default Layout;
