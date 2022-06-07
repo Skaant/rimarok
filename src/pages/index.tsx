5afd60e20cc2655b2470a24c8584a158193615a0
Tim <Tim280696@hotmail.com>
2022-06-07 14:47:20 +0200

    index: import TOC comp + add STYLEGUIDE_HEADERS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,27 @@
 import * as React from "react";
 import Layout from "../components/Layout";
 import Quote from "../components/Quote";
+import TableOfContents from "../components/TableOfContents";
+import type Header from "../types/Header";
 import "../styles/global.scss";
 
 const Accueil = () => {
+  
+  const STYLEGUIDE_HEADERS: Header[] = [
+    {
+      id: "row",
+      title: "Row",
+    },
+    {
+      id: "table-of-contents",
+      title: "Table of contents",
+    },
+    {
+      id: "quote",
+      title: "Quote",
+    },
+  ];
+
   return (
     <Layout
       head={{
