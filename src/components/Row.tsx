52b8cb43f7ca5a63f12705db75c51042c487dc33
Rimarok <romaric.ruga@gmail.com>
2022-06-07 11:06:30 +0200

    Row.tsx: add backgroundColor prop

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,11 +1,17 @@
 import React, { ReactElement } from "react";
+import { COLORS } from "../data/colors";
 
 type RowProps = {
+  backgroundColor?: COLORS;
   children: ReactElement;
 };
 
-function Row({ children }: RowProps) {
-  return <div className="row">{children}</div>;
+function Row({ backgroundColor, children }: RowProps) {
+  return (
+    <div className={`row ${backgroundColor ? `bg-${backgroundColor}` : ""}`}>
+      {children}
+    </div>
+  );
 }
 
 export default Row;
