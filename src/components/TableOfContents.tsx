7470dbf22c49b399373116e66361dcf835b00dd6
Rimarok <romaric.ruga@gmail.com>
2022-06-08 11:13:39 +0200

    TableOfContents: receive header as object instead of array

--- src/components/TableOfContents.tsx
+++ src/components/TableOfContents.tsx
@@ -3,13 +3,13 @@ import * as React from "react";
 import Header from "../types/Header";
 
 type TableOfContentsProps = {
-  contents: Header[];
+  contents: { [key: string]: Header };
 };
 
 function TableOfContents({ contents }: TableOfContentsProps) {
   return (
     <ul>
-      {contents.map(({ id, title }) => (
+      {Object.values(contents).map(({ id, title }) => (
         <li>
           <Link to={`#${id}`}>{title}</Link>
         </li>
