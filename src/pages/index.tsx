38a748e68bcc337a635bde7dae1e90674914160c
Rimarok <romaric.ruga@gmail.com>
2022-06-08 10:44:41 +0200

    index page: Remove STYLEGUIDE_HEADERS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,10 @@
 import * as React from "react";
 import Layout from "../components/Layout";
-import TableOfContents from "../components/TableOfContents";
-import type Header from "../types/Header";
 import Row from "../components/Row";
 import { COLORS } from "../data/colors";
 import "../styles/global.scss";
 
 const Accueil = () => {
-  const STYLEGUIDE_HEADERS: Header[] = [
-    {
-      id: "row",
-      title: "Row",
-    },
-    {
-      id: "table-of-contents",
-      title: "Table of contents",
-    },
-    {
-      id: "quote",
-      title: "Quote",
-    },
-  ];
-
   return (
     <Layout
       head={{
