369cc51a505e9314f782075e5d1a2d9c56c12d4d
Rimarok <romaric.ruga@gmail.com>
2022-06-10 16:43:08 +0200

    Row: add id prop

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -2,13 +2,17 @@ import React, { ReactElement } from "react";
 import { COLORS } from "../data/colors";
 
 type RowProps = {
+  id?: string;
   backgroundColor?: COLORS;
   children: ReactElement;
 };
 
-function Row({ backgroundColor, children }: RowProps) {
+function Row({ id, backgroundColor, children }: RowProps) {
   return (
-    <div className={`row ${backgroundColor ? `bg-${backgroundColor}` : ""}`}>
+    <div
+      id={id}
+      className={`row ${backgroundColor ? `bg-${backgroundColor}` : ""}`}
+    >
       <div className="col-12 col-sm-10 col-md-8 col-lg-6">{children}</div>
     </div>
   );
