c196313e56e18c13be663ecc3d7142c7b1b73f56
Rimarok <romaric.ruga@gmail.com>
2022-06-13 08:10:59 +0200

    index page: Add first CTA content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import type Header from "../types/Header";
 import Row from "../components/Row";
 import { COLORS } from "../data/colors";
 import "../styles/global.scss";
+import Quote from "../components/Quote";
 
 const ACCUEIL_TITLE = "Construire un internet durable";
 
@@ -19,6 +20,69 @@ const Accueil = () => {
           <>
             <h1>{ACCUEIL_TITLE}</h1>
             <p>RIMAROK, ingénierie web et eco-conception</p>
+          </>
+        </Row>
+        <Row>
+          <>
+            <Quote displayHeading={4}>
+              <>
+                <p>
+                  Internet est un espace d’émancipation et de liberté, il
+                  devrait d’abord être soutenable et inclusif.
+                </p>
+              </>
+            </Quote>
+            <p>
+              À 23 ans, <b>en 2016</b>, mon Master Informatique et Gestion
+              d’Entreprise en poche,{" "}
+              <b>
+                j’ai commencé l’activité de développeur web comme indépendant
+              </b>
+              .
+            </p>
+            <div /** @todo Buttons menu */>
+              <button>Découvrir ma prestation</button>
+            </div>
+            <p>
+              Depuis, <b>j’exerce toujours la même activité</b>, en recherchant
+              les projets les plus intéressants et les équipes les plus
+              bienveillantes.
+            </p>
+            <p>
+              Spécialisé <b>full-stack JavaScript</b>, j'ai aussi développé ma{" "}
+              <b>
+                sensibilité au sujet de l’éco-conception et de l’informatique
+                durable
+              </b>
+              .
+            </p>
+            <div /** @todo Buttons menu */>
+              <button>Expertise full-stack JavaScript</button>
+              <button>Bonnes pratiques d'éco-conception</button>
+            </div>
+            <p>J’ai une personnalité créative, vaillante et sociale.</p>
+            <p>On a dit de moi :</p>
+            <Quote
+              displayHeading={6}
+              legend={{
+                author:
+                  "Laurent Gaveau, chef du pôle applicatif à l'Université de Reims",
+              }}
+            >
+              <>
+                <p>
+                  Autonome, volontaire et dynamique, Romaric Ruga dispose
+                  d’excellentes compétences organisationnelles, techniques et
+                  relationnelles. Il maitrise les langages de programmation,
+                  s’est approprié rapidement notre Système d’Informations et
+                  s’est intégré parfaitement dans l’équipe et dans son contexte
+                  professionnel.
+                </p>
+              </>
+            </Quote>
+            <div /** @todo Buttons menu */>
+              <button>Entrer en contact</button>
+            </div>
           </>
         </Row>
       </>
