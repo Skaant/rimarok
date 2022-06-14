b212a0ebcdd2acfbf45495be73109f071b74a33c
Rimarok <romaric.ruga@gmail.com>
2022-06-14 16:25:44 +0200

    index page: FullScreenTitle adapt integration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,9 +18,11 @@ const Accueil = () => {
       }}
     >
       <>
-        <Row backgroundColor={COLORS.FLOWER} col="col-12">
-          <FullScreenTitle title={title} subtitle={WEBSITE_DATA.TITLE} />
-        </Row>
+        <FullScreenTitle
+          title={title}
+          subtitle={WEBSITE_DATA.TITLE}
+          backgroundColor={COLORS.FLOWER}
+        />
         <Row
           header={{
             level: 2,
@@ -31,28 +33,38 @@ const Accueil = () => {
             <Quote displayHeading={4}>
               <>
                 <p>
-                  Internet est un espace d’émancipation et de liberté, il devrait d’abord être
-                  soutenable et inclusif.
+                  Internet est un espace d’émancipation et de liberté, il
+                  devrait d’abord être soutenable et inclusif.
                 </p>
               </>
             </Quote>
             <p>
-              À 23 ans, <b>en 2016</b>, mon Master Informatique et Gestion d’Entreprise en poche,{" "}
-              <b>j’ai commencé l’activité de développeur web comme indépendant</b>.
+              À 23 ans, <b>en 2016</b>, mon Master Informatique et Gestion
+              d’Entreprise en poche,{" "}
+              <b>
+                j’ai commencé l’activité de développeur web comme indépendant
+              </b>
+              .
             </p>
             <div /** @todo Buttons menu */>
               <button>Découvrir ma prestation</button>
             </div>
             <p>
-              Depuis, <b>j’exerce toujours la même activité</b>, en recherchant les projets les plus
-              intéressants et les équipes les plus bienveillantes.
+              Depuis, <b>j’exerce toujours la même activité</b>, en recherchant
+              les projets les plus intéressants et les équipes les plus
+              bienveillantes.
             </p>
             <p>
               Spécialisé <b>full-stack JavaScript</b>, j'ai aussi développé ma{" "}
-              <b>sensibilité au sujet de l’éco-conception et de l’informatique durable</b>.
+              <b>
+                sensibilité au sujet de l’éco-conception et de l’informatique
+                durable
+              </b>
+              .
             </p>
             <p>
-              À la rencontre de ces deux sujets naît une <b>puissante efficience</b>, rencontre de{" "}
+              À la rencontre de ces deux sujets naît une{" "}
+              <b>puissante efficience</b>, rencontre de{" "}
               <b>motifs de conception</b> éprouvés et d'un objectif d'
               <b>économie maximale</b> (ressources informatiques et finance).
             </p>
@@ -65,15 +77,18 @@ const Accueil = () => {
             <Quote
               displayHeading={6}
               legend={{
-                author: "Laurent Gaveau, chef du pôle applicatif à l'Université de Reims",
+                author:
+                  "Laurent Gaveau, chef du pôle applicatif à l'Université de Reims",
               }}
             >
               <>
                 <p>
-                  Autonome, volontaire et dynamique, Romaric Ruga dispose d’excellentes compétences
-                  organisationnelles, techniques et relationnelles. Il maitrise les langages de
-                  programmation, s’est approprié rapidement notre Système d’Informations et s’est
-                  intégré parfaitement dans l’équipe et dans son contexte professionnel.
+                  Autonome, volontaire et dynamique, Romaric Ruga dispose
+                  d’excellentes compétences organisationnelles, techniques et
+                  relationnelles. Il maitrise les langages de programmation,
+                  s’est approprié rapidement notre Système d’Informations et
+                  s’est intégré parfaitement dans l’équipe et dans son contexte
+                  professionnel.
                 </p>
               </>
             </Quote>
