af0115fd73fa90ca3fe9860996446ca67ae8873b
Rimarok <romaric.ruga@gmail.com>
2022-06-14 16:25:16 +0200

    FullScreenTitle: encapsulate Row

--- src/components/FullScreenTitle.tsx
+++ src/components/FullScreenTitle.tsx
@@ -1,16 +1,27 @@
 import * as React from "react";
+import Row, { RowProps } from "./Row";
 
-type FullScreenTitleProps = {
+type FullScreenTitleProps = Pick<RowProps, "backgroundColor"> & {
   title: string;
   subtitle: string;
 };
 
-function FullScreenTitle({ title, subtitle }: FullScreenTitleProps) {
+function FullScreenTitle({
+  backgroundColor,
+  title,
+  subtitle,
+}: FullScreenTitleProps) {
   return (
-    <div className="w-100 vh-100 d-flex flex-column justify-content-center align-items-center text-center p-4">
-      <h1 className="main-title">{title}</h1>
-      <p className="sub-title">{subtitle}</p>
-    </div>
+    <Row
+      col="col-12"
+      colClassName="w-100 vh-100 d-flex flex-column justify-content-center align-items-center text-center p-4"
+      backgroundColor={backgroundColor}
+    >
+      <>
+        <h1 className="main-title">{title}</h1>
+        <p className="sub-title">{subtitle}</p>
+      </>
+    </Row>
   );
 }
 
