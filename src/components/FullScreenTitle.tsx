94655c962f4f930097df4661c4a4062827f1d9ac
Tim <Tim280696@hotmail.com>
2022-06-14 13:21:38 +0200

    FullScreenTitle: remove h4 class

--- src/components/FullScreenTitle.tsx
+++ src/components/FullScreenTitle.tsx
@@ -9,7 +9,7 @@ function FullScreenTitle({ title, subtitle }: FullScreenTitleProps) {
   return (
     <div className="w-100 vh-100 d-flex flex-column justify-content-center align-items-center text-center p-4">
       <h1 className="main-title">{title}</h1>
-      <p className="sub-title h4">{subtitle}</p>
+      <p className="sub-title">{subtitle}</p>
     </div>
   );
 }
