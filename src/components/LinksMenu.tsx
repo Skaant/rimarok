9e20519b62cb6756f2daa29b6d014addce86d7d7
Rimarok <romaric.ruga@gmail.com>
2022-06-15 07:35:11 +0200

    LinksMenu: add item color + item destructu

--- src/components/LinksMenu.tsx
+++ src/components/LinksMenu.tsx
@@ -1,8 +1,14 @@
 import { Link } from "gatsby";
 import * as React from "react";
+import { COLORS } from "../data/colors";
 import Row from "./Row";
 
-export type LinksMenuItem = { link: string; internal?: boolean; label: string };
+export type LinksMenuItem = {
+  link: string;
+  internal?: boolean;
+  label: string;
+  color?: COLORS;
+};
 
 type LinksMenuProps = {
   links: LinksMenuItem[];
@@ -12,24 +18,24 @@ function LinksMenu({ links }: LinksMenuProps) {
   return (
     <div>
       <>
-        {links.map((item, index) =>
-          item.internal ? (
+        {links.map(({ link, internal, label, color = COLORS.FLOWER }, index) =>
+          internal ? (
             <Link
-              to={item.link}
+              to={link}
               key={index}
-              className="btn btn-primary mx-2"
+              className={`btn btn-primary mx-2 bg-${color}`}
               role="button"
             >
-              {item.label}
+              {label}
             </Link>
           ) : (
             <a
-              href={item.link}
+              href={link}
               key={index}
-              className="btn btn-primary mx-2"
+              className={`btn btn-primary mx-2 bg-${color}`}
               role="button"
             >
-              {item.label}
+              {label}
             </a>
           )
         )}
