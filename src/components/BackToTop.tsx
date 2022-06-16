52bf4df99dce137ee6a8e9e80de14584b77c6745
Tim <Tim280696@hotmail.com>
2022-06-16 14:56:53 +0200

    BackToTop: update className + style + add colors

--- src/components/BackToTop.tsx
+++ src/components/BackToTop.tsx
@@ -1,17 +1,20 @@
 import * as React from "react";
+import { COLORS } from "../data/colors";
 
 type BackToTopProps = {
   isFixed?: boolean;
+  backgroundColor?: COLORS;
+  textColor?: COLORS;
 };
 
-function BackToTop({ isFixed = true }: BackToTopProps) {
+function BackToTop({ isFixed = true, backgroundColor, textColor }: BackToTopProps) {
   return (
     <button
       type="button"
       id="top-button"
-      className={
-        isFixed ? "back-to-top-fixed btn rounded-circle" : "back-to-top-relative btn rounded-circle"
-      }
+      className={`btn rounded-circle d-none border-0 ${
+        isFixed ? "position-fixed" : "position-relative"
+      } bottom-0 end-0 m-5 px-3 py-2 bg-${backgroundColor} text-${textColor}`}
     >
       ↑
     </button>
