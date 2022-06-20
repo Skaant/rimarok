1a1f41c5224f95c779e0d813ea056e8281105980
Tim <Tim280696@hotmail.com>
2022-06-20 09:56:02 +0200

    FullScreenTitle: add width and titleSize

--- src/components/FullScreenTitle.tsx
+++ src/components/FullScreenTitle.tsx
@@ -4,21 +4,25 @@ import Row, { RowProps } from "./Row";
 type FullScreenTitleProps = Pick<RowProps, "backgroundColor"> & {
   title: string;
   subtitle: string;
+  width: "100vh" | "60vh";
+  titleSize: "normal" | "big";
 };
 
 function FullScreenTitle({
   backgroundColor,
   title,
   subtitle,
+  width = "60vh",
+  titleSize,
 }: FullScreenTitleProps) {
   return (
     <Row
       col="col-12"
-      colClassName="w-100 vh-100 d-flex flex-column justify-content-center align-items-center text-center p-4"
+      colClassName={`${width} vh-100 d-flex flex-column justify-content-center align-items-center text-center p-4`}
       backgroundColor={backgroundColor}
     >
       <>
-        <h1 className="main-title">{title}</h1>
+        <h1 className={titleSize}>{title}</h1>
         <p className="sub-title">{subtitle}</p>
       </>
     </Row>
