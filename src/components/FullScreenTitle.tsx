7caff188bc01068754981e847478b0f3fe99a18d
Rimarok <romaric.ruga@gmail.com>
2022-07-05 07:22:37 +0200

    FullScreenTitle: remove 100vh variation
    Remove height prop

--- src/components/FullScreenTitle.tsx
+++ src/components/FullScreenTitle.tsx
@@ -4,7 +4,6 @@ import Row, { RowProps } from "./Row";
 type FullScreenTitleProps = Pick<RowProps, "backgroundColor"> & {
   title: string;
   subtitle: string;
-  height?: "100vh" | "60vh";
   titleSize?: "normal" | "big";
 };
 
@@ -12,16 +11,13 @@ function FullScreenTitle({
   backgroundColor,
   title,
   subtitle,
-  height = "60vh",
   titleSize = "normal",
 }: FullScreenTitleProps) {
   return (
     <Row
       id="full-screen-title"
       col="col-12"
-      colClassName={`${
-        height === "60vh" ? "vh-60" : "vh-100"
-      } d-flex flex-column justify-content-center align-items-center text-center p-4`}
+      colClassName="60vh d-flex flex-column justify-content-center align-items-center text-center p-4"
       backgroundColor={backgroundColor}
       paddingY={0}
     >
