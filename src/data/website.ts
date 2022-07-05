c257ae3f5a75e4ba3791f938d89fd6486bea9fd5
Rimarok <romaric.ruga@gmail.com>
2022-07-05 07:25:24 +0200

    data/website: add calendly linkedin & github links

--- src/data/website.ts
+++ src/data/website.ts
@@ -1,6 +1,10 @@
 const WEBSITE_DATA = {
   TITLE: "RIMAROK.com, ingénierie web et éco-conception",
-  BRAND_NAME: "RIMAROK.com"
+  BRAND_NAME: "RIMAROK.com",
+
+  CALENDLY_LINK: "https://calendly.com/rimarok/30min",
+  LINKEDIN_LINK: "https://www.linkedin.com/in/romaricruga/",
+  GITHUB_LINK: "https://github.com/Skaant",
 };
 
 export default WEBSITE_DATA;
