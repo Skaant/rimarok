a78c3256e90f98ed316c72577c9da9a4ace34445
Tim <Tim280696@hotmail.com>
2022-07-13 14:01:28 +0200

    ArticlesList: update sort func + add articles link

--- src/components/ArticlesList.tsx
+++ src/components/ArticlesList.tsx
@@ -1,33 +1,42 @@
 import * as React from "react";
 import { ARTICLES_DATA } from "../data/articles";
 import TagsDateHeader from "./TagsDateHeader";
+import { Link } from "gatsby";
 
 function ArticlesList() {
-  ARTICLES_DATA.sort(function (a, b) {
-    const newA = a.date.split("/").reverse().join("");
-    const newB = b.date.split("/").reverse().join("");
-    return newB.localeCompare(newA);
+  const ARTICLES_DATA_COPY = ARTICLES_DATA.slice();
+
+  ARTICLES_DATA_COPY.sort(function (article1, article2) {
+    return article2.date
+      .split("/")
+      .reverse()
+      .join("")
+      .localeCompare(article1.date.split("/").reverse().join(""));
   });
 
   return (
     <ul className="list-group list-group-flush p-0">
-      {ARTICLES_DATA.map(({ title, description, date, tags }, index) => {
-        return (
-          <li key={index} className="list-group-item list-unstyled my-5">
-            <TagsDateHeader date={date} tags={tags} />
-            <h3>{title}</h3>
-            {description ? (
-              <p>
-                {description.length > 150
-                  ? description.substring(0, 150) + "..."
-                  : description}
-              </p>
-            ) : (
-              ""
-            )}
-          </li>
-        );
-      })}
+      {ARTICLES_DATA_COPY.map(
+        ({ id, title, description, date, tags }, index) => {
+          return (
+            <li key={index} className="list-group-item list-unstyled my-5">
+              <Link to={id} style={{ color: "#212529" }}>
+                <TagsDateHeader date={date} tags={tags} />
+                <h3>{title}</h3>
+                {description ? (
+                  <p>
+                    {description.length > 150
+                      ? description.substring(0, 150) + "..."
+                      : description}
+                  </p>
+                ) : (
+                  ""
+                )}
+              </Link>
+            </li>
+          );
+        }
+      )}
     </ul>
   );
 }
