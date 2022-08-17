85d42c14771d2af4da8c8bd6dbd60dc59ee889ab
Rimarok <romaric.ruga@gmail.com>
2022-08-17 19:21:11 +0200

    colors: add greeve + light/dark

--- src/data/colors.ts
+++ src/data/colors.ts
@@ -4,8 +4,11 @@ export enum COLORS {
   ABYSS = "abyss",
   SUN = "sun",
   FLOWER = "flower",
+  GREEVE = "greeve",
+  LIGHT_GREEVE = "light_greeve",
   LIGHT_MIST = "light_mist",
   LIGHT_SUN = "light_sun",
+  DARK_GREEVE = "dark_greeve",
   /** Bootstrap defaults */
   WHITE = "white",
   BLACK = "black",
