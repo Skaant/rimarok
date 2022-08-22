fe1825c1faa4a945bc585b1471af75a33054383c
Rimarok <romaric.ruga@gmail.com>
2022-08-22 22:47:23 +0200

    apps/prestation : fix error btn undefined

--- src/apps/prestation.js
+++ src/apps/prestation.js
@@ -1,15 +1,21 @@
-const contactButtons = document.querySelectorAll("#contact .links-menu .btn");
-const mailButton = contactButtons[1];
-const phoneButton = contactButtons[2];
 
-[mailButton, phoneButton].forEach((btn) =>
-  btn.addEventListener("click", () => {
-    if (btn === mailButton) {
-      btn.textContent = "romaric.ruga@mailo.fr";
-      btn.href = "mailto:romaric.ruga@mailo.fr";
-    } else if (btn === phoneButton) {
-      btn.textContent = "06 38 24 85 07";
-      btn.href = "tel:0638248507";
-    }
-  })
-);
+if (window.location.pathname === '/prestation') {
+  const contactButtons = document.querySelectorAll("#contact .links-menu .btn");
+  const mailButton = contactButtons[1];
+  const phoneButton = contactButtons[2];
+
+  [mailButton, phoneButton].forEach((btn) =>
+    btn.addEventListener("click", ev => {
+      if (btn.href = '#') {
+        ev.preventDefault()
+        if (btn === mailButton) {
+          btn.textContent = "romaric.ruga@mailo.fr";
+          btn.href = "mailto:romaric.ruga@mailo.fr";
+        } else if (btn === phoneButton) {
+          btn.textContent = "06 38 24 85 07";
+          btn.href = "tel:0638248507";
+        }
+      }
+    })
+  );
+}
