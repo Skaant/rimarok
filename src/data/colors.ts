68e5f21643a58a3d182661f40958852ba978ded0
Rimarok <romaric.ruga@gmail.com>
2022-09-02 11:18:54 +0200

    data/colors: add GROUND + deprecated 6

--- src/data/colors.ts
+++ src/data/colors.ts
@@ -1,14 +1,21 @@
 export enum COLORS {
+  SUN = "sun",
+  FLOWER = "flower",
   MIST = "mist",
   LAGOON = "lagoon",
   ABYSS = "abyss",
-  SUN = "sun",
-  FLOWER = "flower",
+  GROUND = "ground",
+  /** @deprecated */
   GREEVE = "greeve",
+  /** @deprecated */
   LIGHT_GREEVE = "light_greeve",
+  /** @deprecated */
   LIGHT_MIST = "light_mist",
+  /** @deprecated */
   LIGHT_SUN = "light_sun",
+  /** @deprecated */
   DARK_GREEVE = "dark_greeve",
+  /** @deprecated */
   ROSAM = "rosam",
   /** Bootstrap defaults */
   WHITE = "white",
