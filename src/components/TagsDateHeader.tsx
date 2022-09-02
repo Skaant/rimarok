e1d25ccc6395465a2903a168f549fd4faa1f4233
Rimarok <romaric.ruga@gmail.com>
2022-09-02 10:56:34 +0200

    TagsDateHeader: add className prop

--- src/components/TagsDateHeader.tsx
+++ src/components/TagsDateHeader.tsx
@@ -6,11 +6,16 @@ type TagsDateHeaderProps = {
   date?: string;
   scope?: "full-stack" | "front-end";
   tags?: string[];
+  className?: string;
 };
 
-function TagsDateHeader({ date, scope, tags }: TagsDateHeaderProps) {
+function TagsDateHeader({ date, scope, tags, className }: TagsDateHeaderProps) {
   return (
-    <p className="card-text order-0 d-flex justify-content-sm-between align-items-end flex-sm-row flex-column text-start mb-2">
+    <p
+      className={`card-text order-0 d-flex justify-content-sm-between align-items-end flex-sm-row flex-column text-start mb-2${
+        className ? ` ${className}` : ""
+      }`}
+    >
       {
         <BadgesList
           badges={[scope ? scope : "", ...(tags || [])].map((value, index) => ({
