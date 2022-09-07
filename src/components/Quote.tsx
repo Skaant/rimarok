7af02331c3523d5b82d83555e4d0000745df9f9e
rimarok <romaric.ruga@mailo.fr>
2022-09-07 22:17:30 +0200

    Quote : contrast text colors

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from "react";
+import { COLORS } from "../data/colors";
 
 type QuoteProps = {
   displayHeading: 1 | 2 | 3 | 4 | 5 | 6;
@@ -6,13 +7,26 @@ type QuoteProps = {
     source?: string;
     author?: string;
   };
+  backgroundColor?: COLORS;
   children: ReactElement;
   className?: string;
 };
 
-function Quote({ children, displayHeading, legend, className }: QuoteProps) {
+function Quote({
+  children,
+  displayHeading,
+  legend,
+  backgroundColor = COLORS.MIST,
+  className,
+}: QuoteProps) {
   return (
-    <blockquote className={`blockquote${className ? ` ${className}` : ""}`}>
+    <blockquote
+      className={`blockquote my-5 bg-${backgroundColor} text-${
+        [COLORS.MIST, COLORS.GROUND].includes(backgroundColor)
+          ? COLORS.DARK
+          : COLORS.LIGHT
+      } ${className ? ` ${className}` : ""}`}
+    >
       <div className={`display-${displayHeading} font-slab p-4`}>
         {children}
       </div>
