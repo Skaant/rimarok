e21df9031fe42532e00183884730aaa7378d2e6e
rimarok <romaric.ruga@mailo.fr>
2022-09-12 19:12:36 +0200

    TagsDateHeader : allow badges color setting

--- src/components/TagsDateHeader.tsx
+++ src/components/TagsDateHeader.tsx
@@ -7,9 +7,16 @@ type TagsDateHeaderProps = {
   scope?: "full-stack" | "front-end";
   tags?: string[];
   className?: string;
+  badgesColor?: COLORS;
 };
 
-function TagsDateHeader({ date, scope, tags, className }: TagsDateHeaderProps) {
+function TagsDateHeader({
+  date,
+  scope,
+  tags,
+  className,
+  badgesColor = COLORS.FLOWER,
+}: TagsDateHeaderProps) {
   return (
     <p
       className={`card-text order-0 d-flex justify-content-sm-between align-items-end flex-sm-row flex-column text-start mb-2${
@@ -20,7 +27,7 @@ function TagsDateHeader({ date, scope, tags, className }: TagsDateHeaderProps) {
         <BadgesList
           badges={[...(scope ? [scope] : []), ...(tags || [])].map(
             (value, index) => ({
-              color: index || !scope ? COLORS.FLOWER : COLORS.ABYSS,
+              color: index || !scope ? badgesColor : COLORS.ABYSS,
               label: value,
             })
           )}
