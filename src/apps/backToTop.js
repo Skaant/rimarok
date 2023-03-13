f329d2f6ffa6e32461ad63889a14da2a502121f7
rimarok <romaric.ruga@mailo.fr>
2023-03-13 16:34:42 +0100

    fix contactGuards + group init scripts

--- src/apps/backToTop.js
+++ src/apps/backToTop.js
@@ -1,21 +1,23 @@
-const topButtons = document.getElementsByClassName("top-button");
+export function backToTop() {
+  const topButtons = document.getElementsByClassName("top-button");
 
-[...topButtons].forEach((btn) =>
-  btn.addEventListener("click", () => {
-    window.scrollTo(0, 0);
-  })
-);
+  [...topButtons].forEach((btn) =>
+    btn.addEventListener("click", () => {
+      window.scrollTo(0, 0);
+    })
+  );
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY < 200) {
-    if (topButtons[0].classList.contains("d-block")) {
-      topButtons[0].classList.add("d-none");
-      topButtons[0].classList.remove("d-block");
+  window.addEventListener("scroll", () => {
+    if (window.scrollY < 200) {
+      if (topButtons[0].classList.contains("d-block")) {
+        topButtons[0].classList.add("d-none");
+        topButtons[0].classList.remove("d-block");
+      }
+    } else {
+      if (topButtons[0].classList.contains("d-none")) {
+        topButtons[0].classList.add("d-block");
+        topButtons[0].classList.remove("d-none");
+      }
     }
-  } else {
-    if (topButtons[0].classList.contains("d-none")) {
-      topButtons[0].classList.add("d-block");
-      topButtons[0].classList.remove("d-none");
-    }
-  }
-});
+  });
+}
